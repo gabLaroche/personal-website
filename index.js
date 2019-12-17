74bc831513d999ed1acdd355229c1a35ebc23ff0
Gabriel Laroche <gabriel.laroche@nurun.com>
2019-12-17 08:11:58 -0500

    Update locals + redirect

--- index.js
+++ index.js
@@ -18,15 +18,72 @@ app.post('/post', function(request, response) {
     response.send(request.body);
 });
 
-app.get('/', function (req, res) {
-    const enJson = require('./locales/home/en.json');
-    res.render('home', enJson);
+app.get('/', (req, res) => {
+    var lang = req.acceptsLanguage('fr', 'en');
+    if (lang) {
+        res.redirect(`/${lang}`);
+    } else {
+        res.redirect('/en');
+    }
 });
 
-app.get('/fr', function (req, res) {
+app.get('/en', (req, res) => {
+    const enJson = require('./locales/home/en.json');
+    res.render('home', enJson);
+    /*const testProject = {projects: [
+            {
+                "title": "IE11 Death Countdown",
+                "demoUrl": "https://death-to-ie11.netlify.com",
+                "sourceMsg": "Source code",
+                "sourceUrl": "https://github.com/gablaroche/death-to-ie11"
+            },
+            {
+                "title": "Simple UI for the Bored API",
+                "demoUrl": "https://bored-ui.netlify.com/",
+                "sourceMsg": "Source code",
+                "sourceUrl": "https://github.com/gablaroche/bored-ui"
+            },
+            {
+                "title": "Simple Case Converter",
+                "demoUrl": "https://simple-case-conversion.netlify.com",
+                "sourceMsg": "Source code",
+                "sourceUrl": "https://github.com/gabLaroche/simple-text-conversion"
+            },
+            {
+                "title": "Lyrics CLI",
+                "demoUrl": "https://www.npmjs.com/package/my-lyrics-cli",
+                "sourceMsg": "Source code",
+                "sourceUrl": "https://github.com/gablaroche/lyrics-cli"
+            }
+        ]}
+    res.render('home', {
+        pageContent: enJson,
+        projects: testProject
+    });*/
+});
+
+app.get('/fr', (req, res) => {
     const frJson = require('./locales/home/fr.json');
     res.render('home', frJson);
 });
+
+/** START Redirect legacy site to proper urls **/
+app.get('/death-to-ie11', (req, res) => {
+    res.redirect('https://death-to-ie11.netlify.com');
+});
+
+app.get('/bored', (req, res) => {
+    res.redirect('https://bored-ui.netlify.com/');
+});
+
+app.get('/simple-toolkit', (req, res) => {
+    res.redirect('https://simple-toolkit.netlify.com');
+});
+
+app.get('/simple-toolkit/simple-case-conversion', (req, res) => {
+    res.redirect('https://simple-case-conversion.netlify.com');
+});
+/** END Redirect legacy site to proper urls **/
 
 app.set('views', path.join(__dirname, 'views/'));
 app.use(express.static(__dirname + '/public'));
